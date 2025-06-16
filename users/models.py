9f9f1123b44a2565502300b80d54b19cc96e3df3
caiodansk <124446768+caiodansk@users.noreply.github.com>
2025-06-16 15:17:36 -0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Seus campos personalizados aqui
+    
+    class Meta:
+        db_table = 'custom_user'  # Nome personalizado para a tabela no banco de dados
+    
+    # Adicione esses campos para resolver o conflito
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='custom_user_set',  # Nome personalizado
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='custom_user_set',  # Nome personalizado
+        related_query_name='user',
+    )
